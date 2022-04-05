844f7b473f81d62d4b1e26915afef97e6e75782f
Herli Khoury <herli.kh@gmail.com>
2022-04-05 18:29:21 -0300

    endpoint-teste_listener-servidor rodando

--- data-api/index.js
+++ data-api/index.js
@@ -2,3 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/gondor', (req, res) => {
+    res.status(200).send('Gondor calls for aid!');
+    return;
+});
+
+app.listen({port:8080}, () => {
+    console.log('Servidor rodando liso!');
+});
